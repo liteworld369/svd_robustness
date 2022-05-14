5b5aafa5f0ea9d9eeeedba6ac1bc255a71af2418
liteworld369 <al-najjar.ammar@o365.u-szeged.hu>
2022-05-14 10:57:38 +0200

    minor update

--- defenses/mnist/run_mnist.sh
+++ defenses/mnist/run_mnist.sh
@@ -1,9 +1,8 @@
 for method in svd; do
 	for comps in 16 23 34 53 103 281 458; do  
-		for denses in 0 1 2 3 4; do
-			for dense_size in 128 256; do 
-				echo "python train.py --save_dir MNIST --dataset MNIST --epoch 100 --comps $comps --method $method --freeze 0 --normalize1 0 --normalize2 0 --denses $denses --dense_size $dense_size"
-				echo "python train.py --save_dir MNIST --dataset MNIST --epoch 100 --comps $comps --method $method --freeze 1 --normalize1 0 --normalize2 0 --denses $denses --dense_size $dense_size"
+		for denses in 0 1 2 4; do
+			for dense_size in 256; do 
+				eval $(echo "python train.py --save_dir MNIST --dataset MNIST --epoch 100 --comps $comps --method $method --freeze 1 --reconstruct 2 --normalize1 0 --normalize2 0 --denses $denses --dense_size $dense_size") 
 			done  
 		done
 	done   
